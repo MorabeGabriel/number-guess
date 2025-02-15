fc48fda1d07efd9c046f3d94996c928a5fba6909
MorabeGabriel M <daniellacayanan2@gmail.com>
2025-02-15 17:10:32 +0000

    feat: create function to input name and input number guess

--- number_guess.sh
+++ number_guess.sh
@@ -1,51 +1,63 @@
 #!/bin/bash
-
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
-# Generate a random number between 1 and 1000
-SECRET_NUMBER=$(( RANDOM % 1000 + 1 ))
+INPUT_NAME() {
+  echo "Enter your username:"
+  read NAME
+  n=${#NAME}
 
-echo "Enter your username:"
-read USERNAME
-
-# Check if user exists
-USER_DATA=$($PSQL "SELECT user_id, games_played, best_game FROM users WHERE username='$USERNAME'")
-
-if [[ -z $USER_DATA ]]; then
-  echo "Welcome, $USERNAME! It looks like this is your first time here."
-  $PSQL "INSERT INTO users(username, games_played, best_game) VALUES('$USERNAME', 0, NULL)"
-  USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
-  GAMES_PLAYED=0
-  BEST_GAME=NULL
-else
-  IFS="|" read USER_ID GAMES_PLAYED BEST_GAME <<< "$USER_DATA"
-  echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
-fi
-
-echo "Guess the secret number between 1 and 1000:"
-GUESS_COUNT=0
-
-while true; do
-  read GUESS
-  ((GUESS_COUNT++))
-
-  if ! [[ $GUESS =~ ^[0-9]+$ ]]; then
-    echo "That is not an integer, guess again:"
-  elif (( GUESS > SECRET_NUMBER )); then
-    echo "It's lower than that, guess again:"
-  elif (( GUESS < SECRET_NUMBER )); then
-    echo "It's higher than that, guess again:"
+  #When you run your script, you should prompt the user for a username with Enter your username:, and take a username as input.Your database should allow usernames that are 22 characters
+  if [[ ! $n -le 22 ]] || [[ ! $n -gt 0 ]]
+  then
+    INPUT_NAME
   else
-    echo "You guessed it in $GUESS_COUNT tries. The secret number was $SECRET_NUMBER. Nice job!"
-    
-    ((GAMES_PLAYED++))
-    $PSQL "UPDATE users SET games_played=$GAMES_PLAYED WHERE user_id=$USER_ID"
-
-    if [[ -z $BEST_GAME || $GUESS_COUNT -lt $BEST_GAME ]]; then
-      $PSQL "UPDATE users SET best_game=$GUESS_COUNT WHERE user_id=$USER_ID"
+    USER_NAME=$(echo $($PSQL "SELECT username FROM users WHERE username='$NAME';") | sed 's/ //g')
+    if [[ ! -z $USER_NAME ]]
+    then
+      #If that username has been used before, it should print Welcome back, <username>! You have played <games_played> games, and your best game took <best_game> guesses., 
+      #with <username> being a users name from the database,
+      #<games_played> being the total number of games that user has played, 
+      #and <best_game> being the fewest number of guesses it took that user to win the game
+      USER_ID=$(echo $($PSQL "SELECT user_id FROM users WHERE username='$USER_NAME';") | sed 's/ //g')
+      USER_NAME=$(echo $($PSQL "SELECT username FROM users WHERE user_id='$USER_ID';") | sed 's/ //g')
+      GAME_PLAYED=$(echo $($PSQL "SELECT frequent_games FROM users WHERE user_id=$USER_ID;") | sed 's/ //g')
+      BEST_GAME=$(echo $($PSQL "SELECT MIN(best_guess) FROM users LEFT JOIN games USING(user_id) WHERE user_id=$USER_ID;") | sed 's/ //g')
+      echo "Welcome back, $USER_NAME! You have played $GAME_PLAYED games, and your best game took $BEST_GAME guesses."
+    else
+      #If the username has not been used before, you should print Welcome, <username>! It looks like this is your first time here.
+      USER_NAME=$NAME
+      echo -e "\nWelcome, $USER_NAME! It looks like this is your first time here."
     fi
 
-    break
+    #Your script should randomly generate a number that users have to guess
+    #The next line printed should be Guess the secret number between 1 and 1000: and input from the user should be read
+    CORRECT_ANSWER=$(( $RANDOM % 1000 + 1 ))
+    GUESS_COUNT=0
+    INPUT_GUESS $USER_NAME $CORRECT_ANSWER $GUESS_COUNT
   fi
-done
+}
 
+INPUT_GUESS() {
+  USER_NAME=$1
+  CORRECT_ANSWER=$2
+  GUESS_COUNT=$3
+  USSER_GUESS=$4
+
+  if [[ -z $USSER_GUESS ]]
+  then
+    echo "Guess the secret number between 1 and 1000:"
+    read USSER_GUESS
+  else
+    #If anything other than an integer is input as a guess, it should print That is not an integer, guess again:
+    echo "That is not an integer, guess again:"
+    read USSER_GUESS
+  fi
+
+  GUESS_COUNT=$(( $GUESS_COUNT + 1 ))
+  if [[ ! $USSER_GUESS =~ ^[0-9]+$ ]]
+  then
+    INPUT_GUESS $USER_NAME $CORRECT_ANSWER $GUESS_COUNT $USSER_GUESS
+  else
+    CHECK_ANSWER $USER_NAME $CORRECT_ANSWER $GUESS_COUNT $USSER_GUESS
+  fi
+}
